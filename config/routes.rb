20f484a4930feec7b869f8209286885a22dffe8c
xiyini <Q2z@HotmaiL.CoM>
2022-03-06 23:58:02 +0400

    Update Devise controller target

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  devise_for :members
+  devise_for :members, controllers: { registrations: 'registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
